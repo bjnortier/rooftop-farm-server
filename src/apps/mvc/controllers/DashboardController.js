a65cacdf541b37119ad0c609a13345958f27f287
Ben Nortier <bjnortier@gmail.com>
2016-02-25 17:31:30 +0200

    Add data fetch

--- src/apps/mvc/controllers/DashboardController.js
+++ src/apps/mvc/controllers/DashboardController.js
@@ -1,12 +1,29 @@
 const trip = require('triptych');
 const Model = trip.Model;
 const Controller = trip.Controller;
+const request = require('browser-request');
 
 class DashboardController extends Controller {
 
   constructor() {
     super(new Model());
-    console.log('hello world');
+    request({url: '/api/measurements', json: true,
+      qs: {sensor_id: 'moist_a'}},
+      (err, response, body) => {
+        if (err) {
+          console.error(err);
+        } else {
+          let parsed = body.map((raw) => {
+            return {
+              timestamp: new Date(raw.timestamp),
+              data: JSON.parse(raw.data)
+            };
+          });
+          console.info(parsed.map((m) => {
+            return m.data.value;
+          }));
+        }
+      });
   }
 
 }
